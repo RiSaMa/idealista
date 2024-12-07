9f5f7e21df4bf4fb86830c8d3f4e8b2ade40a17d
Ricardo Sanchez Matilla <ricardosm89@gmail.com>
2024-12-07 12:05:12 +0000

    sync

--- code/dropbox_utils.py
+++ code/dropbox_utils.py
@@ -1,13 +1,33 @@
 import os
 import dropbox
+from dropbox.oauth import DropboxOAuth2FlowNoRedirect
 import pandas as pd 
 
+APP_KEY=os.getenv('DROPBOX_APP_KEY'),
+APP_SECRET=os.getenv('DROPBOX_APP_SECRET')
+
+def get_dropbox_refresh_token():
+    auth_flow = DropboxOAuth2FlowNoRedirect(APP_KEY, APP_SECRET, token_access_type='offline')
+    authorize_url = auth_flow.start()
+
+    print("1. Go to: " + authorize_url)
+    print("2. Click 'Allow' (you might have to log in first)")
+    print("3. Copy the authorization code.")
+
+    auth_code = input("Enter the authorization code here: ").strip()
+    oauth_result = auth_flow.finish(auth_code)
+
+    print("Access Token:", oauth_result.access_token)
+    print("Refresh Token:", oauth_result.refresh_token)
+    return oauth_result.refresh_token
+
 def get_dropbox_client():
+    DROPBOX_REFRESH_TOKEN = get_dropbox_refresh_token()
     dbx = dropbox.Dropbox(
         #os.getenv('DROPBOX_TOKEN')
         app_key=os.getenv('DROPBOX_APP_KEY'),
         app_secret=os.getenv('DROPBOX_APP_SECRET'),
-        #oauth2_refresh_token=os.getenv('DROPBOX_REFRESH_TOKEN')
+        oauth2_refresh_token=DROPBOX_REFRESH_TOKEN
     )
     return dbx
 
